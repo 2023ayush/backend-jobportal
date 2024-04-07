e288f897fa015cd9c323d6d32a31760a582399ff
Ayush Guragain <guragain.aayush7@gmail.com>
2024-04-07 21:26:41 +0545

    routes & db_conn

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
+import userRouter from "./routes/userRouter.js";
+import jobRouter from "./routes/jobRouter.js";
+import applicationRouter from "./routes/applicationRouter.js";
+import { dbConnection } from "./database/dbConnection.js";
 
 const app = express();
 dotenv.config({ path: "./config/config.env" });
@@ -22,5 +26,9 @@ app.use(
     tempFileDir: "/tmp/",
   })
 );
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/application", applicationRouter);
+app.use("/api/v1/job", jobRouter);
 
+dbConnection();
 export default app;
