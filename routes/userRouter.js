1737ca9a66433887172ee545622272ae06c00ea0
Ayush Guragain <guragain.aayush7@gmail.com>
2024-04-09 16:28:55 +0545

    user authentication

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { logout, register } from "../controllers/userController.js";
 import { login } from "../controllers/userController.js";
+import { isAuthorized } from "../middlewares/auth.js";
 const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
-router.get("/logout", logout);
+router.get("/logout", isAuthorized, logout);
 
 export default router;
