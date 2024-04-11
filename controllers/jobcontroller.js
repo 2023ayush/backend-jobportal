53749af3e020dcfe4487e014d174b98ddba3c0f1
Ayush Guragain <guragain.aayush7@gmail.com>
2024-04-11 14:05:39 +0545

    update job

--- controllers/jobcontroller.js
+++ controllers/jobcontroller.js
@@ -83,3 +83,28 @@ export const getmyJobs = catchAsyncError(async (req, res, next) => {
     myjobs,
   });
 });
+
+//UPDATE JOB
+export const updateJob = catchAsyncError(async (req, res, next) => {
+  const { role } = req.user;
+  if (role === "Job Seeker") {
+    return next(
+      new ErrorHandler("Job Seeker not allowed to access this resource.", 400)
+    );
+  }
+  const { id } = req.params;
+  let job = await Job.findById(id);
+  if (!job) {
+    return next(new ErrorHandler("Oops, Job not found", 400));
+  }
+  job = await Job.findByIdAndUpdate(id, req.body, {
+    new: true,
+    runValidators: true,
+    userFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+    job,
+    message: "Job Updated Successfully",
+  });
+});
--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -3,10 +3,13 @@ import {
   postJob,
   getAllJobs,
   getmyJobs,
+  updateJob,
 } from "../controllers/jobcontroller.js";
 import { isAuthorized } from "../middlewares/auth.js";
 const router = express.Router();
 router.post("/post", isAuthorized, postJob);
 router.get("/getall", isAuthorized, getAllJobs);
 router.get("/getmyjob", isAuthorized, getmyJobs);
+router.put("/update/:id", isAuthorized, updateJob);
+
 export default router;
