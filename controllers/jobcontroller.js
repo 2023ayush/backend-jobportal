a78e80e0c6e5304c67868cd0fe17da3a822d8a31
Ayush Guragain <guragain.aayush7@gmail.com>
2024-04-11 14:19:37 +0545

    delete job

--- controllers/jobcontroller.js
+++ controllers/jobcontroller.js
@@ -108,3 +108,23 @@ export const updateJob = catchAsyncError(async (req, res, next) => {
     message: "Job Updated Successfully",
   });
 });
+
+//DELETE JOB
+export const deleteJob = catchAsyncError(async (req, res, next) => {
+  const { role } = req.user;
+  if (role === "Job Seeker") {
+    return next(
+      new ErrorHandler("Job Seeker not allowed to access this resource.", 400)
+    );
+  }
+  const { id } = req.params;
+  let job = await Job.findById(id);
+  if (!job) {
+    return next(new ErrorHandler("Job not found", 404));
+  }
+  await job.deleteOne();
+  res.status(200).json({
+    success: true,
+    message: "Job deleted successfully",
+  });
+});
--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -4,6 +4,7 @@ import {
   getAllJobs,
   getmyJobs,
   updateJob,
+  deleteJob,
 } from "../controllers/jobcontroller.js";
 import { isAuthorized } from "../middlewares/auth.js";
 const router = express.Router();
@@ -11,5 +12,6 @@ router.post("/post", isAuthorized, postJob);
 router.get("/getall", isAuthorized, getAllJobs);
 router.get("/getmyjob", isAuthorized, getmyJobs);
 router.put("/update/:id", isAuthorized, updateJob);
+router.delete("/delete/:id", isAuthorized, deleteJob);
 
 export default router;
